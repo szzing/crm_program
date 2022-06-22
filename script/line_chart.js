726c8be02cc366cdbe5e546376d4f61b5afd8bc3
szzing <93658676+szzing@users.noreply.github.com>
2022-06-22 18:13:53 +0900

    구글 차트 선그래프 작업 (임의데이터)

---
+++ script/line_chart.js
@@ -0,0 +1,49 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+var data = google.visualization.arrayToDataTable([
+  ['X축', 'Sales', 'Expenses'],
+  [0, 0, 0],
+  [1, 1, 2],
+  [2, 5, 1],
+  [3, 4, 2],
+  [4, 7, 4],
+  [5, 2, 3],
+  [6, 1, 2]
+]);
+
+var options = {
+  height : 350,
+  backgroundColor : '#121119',
+  curveType: 'function',
+  legend: {position:'none'},
+  hAxis: {
+    textStyle: {color: '#261e3a'},
+    baseline:'none',
+    gridlines: {
+      color: 'transparent',
+    }
+  },
+  vAxis: {
+    textStyle: {color: '#261e3a'},
+    baseline: {color: '#261e3a'},
+    gridlines: {
+      color: '#261e3a'
+    },
+    viewWindowMode:'explicit',
+    viewWindow: {
+        min: 0,
+        max: 8
+    }
+  },
+  lineWidth: 5,
+  series: {
+    0: { color: '#ef4766'},
+    1: { color: '#4bc5d6'}
+  }
+};
+
+var chart = new google.visualization.LineChart(document.getElementById('myChart'));
+  chart.draw(data, options);
+}
